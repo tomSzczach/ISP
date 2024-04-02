b64c6e8c424ead7d22e355b619f70f108c8d4191
kosskas <93159483+kosskas@users.noreply.github.com>
2024-04-02 18:50:43 +0200

    Finished lab 9

--- lab9/design.h
+++ lab9/design.h
@@ -2,30 +2,27 @@
 
 SC_MODULE (gray) {
 // Input and output signals:
-  sc_in<bool > clk_i;
-  sc_in<sc_logic > rst_i;
+  sc_in<bool > clock;
+  sc_in<bool> reset;
   sc_out<sc_uint<3> > led_o;
 
   //------------Local Variables Here---------------------
-  sc_uint<3>	gray_encoder;
-  int q = 0;
+  //sc_uint<3>	d;
+  sc_uint<3>	q;
 
   //------------Code Starts Here-------------------------
   // Below function implements actual counter logic
   void gray_process () {
     // At every rising edge of clock we check if reset is active    
-    if (rst_i.read() == 1) {
-      count =  0;
-      count2 =  0;
-      led_o.write(0);    
-    }     
-    else {
-		q++;
-		gray_encoder[2] = (q >> 2) & 1;
-		gray_encoder[1] = ((q >> 1) ^ (q >> 2)) & 1;
-		gray_encoder[0] = (q ^ (q >> 1)) & 1;		
-	}
-	led_o.write(gray_encoder);
+    if (reset.read() == 1) {
+		q = 0;    
+    }  
+    else if (clock.posedge()) {
+		q = q + 1; // Increment the counter value
+    }	
+	//gray = liczba ^ (liczba >> 1)
+    //000, 001, 011, 010, 110, 111, 101, 100
+    led_o.write(q ^ (q >> 1)); // Gray code encoding
   }
 
   // Constructor for the counter
@@ -34,10 +31,9 @@ SC_MODULE (gray) {
   // edge of the clock and also when ever reset changes state
   SC_CTOR(gray) {
 	led_o.initialize(0);
-	q=0;
     cout<<"Executing new"<<endl;
     SC_METHOD(gray_process);
-    sensitive << rst_i.pos();
+    sensitive << reset.pos();
     sensitive << clock.pos();
   } // End of Constructor
 
--- lab9/testbench.cpp
+++ lab9/testbench.cpp
@@ -6,33 +6,49 @@
 #include "design.h"
 
 int sc_main (int argc, char* argv[]) {
-  sc_signal<bool>   rst_i;
-  sc_signal<sc_uint<8> > led_o;
+  sc_signal<bool>   clock;
+  sc_signal<bool>   reset;
+  sc_signal<sc_uint<3> > led_o;
   int i = 0;
-  // Create clock (period=10 ns  duty=0.5  delay=2 ns):
-  sc_clock clk_i("clk_i",10,SC_NS,0.5,2,SC_NS,false);
-  
   // Connect the DUT
   gray counter("COUNTER");
-  counter.clk_i(clk_i);
-  counter.rst_i(rst_i);
+  counter.clock(clock);
+  counter.reset(reset);
   counter.led_o(led_o);
 
-
+  sc_start(1, SC_NS);
 
   // Open VCD file
   sc_trace_file *wf = sc_create_vcd_trace_file("counter");
   // Dump the desired signals
-  sc_trace(wf, clk_i, "clk_i");
-  sc_trace(wf,rst_i, "rst_i");
+  sc_trace(wf, clock, "clock");
+  sc_trace(wf, reset, "reset");
   sc_trace(wf, led_o, "led_o");
 
   // Initialize all variables
-  rst_i = SC_LOGIC_0;       // initial value of reset
-  sc_start(4, SC_NS);
-  
-  rst_i = SC_LOGIC_1;
-  sc_start(40, SC_NS);
+  reset = 0;       // initial value of reset
+  for (i=0;i<50;i++) {
+    clock = 0; 
+    sc_start(1, SC_NS);
+    clock = 1; 
+    sc_start(1, SC_NS);
+  }
+  reset = 1;    // Assert the reset
+  cout << "@" << sc_time_stamp() <<" Asserting reset\n" << endl;
+  for (i=0;i<50;i++) {
+    clock = 0; 
+    sc_start(1, SC_NS);
+    clock = 1; 
+    sc_start(1, SC_NS);
+  }
+  reset = 0;    // De-assert the reset
+  cout << "@" << sc_time_stamp() <<" De-Asserting reset\n" << endl;
+  for (i=0;i<500;i++) {
+    clock = 0; 
+    sc_start(1, SC_NS);
+    clock = 1; 
+    sc_start(1, SC_NS);
+  }
   cout << "@" << sc_time_stamp() <<" Terminating simulation\n" << endl;
   sc_close_vcd_trace_file(wf);
   return 0;// Terminate simulation
